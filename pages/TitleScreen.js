e186ec56640fb196f8865e9e0e38d7e86fc68af7
Christy Lo <christylo@Christys-MacBook-Air.local>
2021-09-18 09:13:00 -0700

    finalized title screen

--- pages/TitleScreen.js
+++ pages/TitleScreen.js
@@ -2,13 +2,21 @@ import * as React from 'react';
 import { View, ImageBackground } from 'react-native';
 import { Pressable } from 'react-native';
 import Logo from '../assets/logo.svg';
+import Background from '../assets/logo-background.svg';
 
 export function TitleScreen({ navigation }) {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ImageBackground source={require('../assets/logo-background.png')} resizeMode="cover" style={{width: '100%', height: '100%', justifyContent: 'center'}}>
+        <ImageBackground source={require('../assets/logo-background.png')} resizeMode="cover" style={{width: '100%', height: '100%', justifyContent: 'center', blend: 'pass through'}}>
             <Pressable onPress={() => navigation.navigate('Home')}>
-                <Logo flex={1} flexDirection={'column'} justifyContent={'center'} alignSelf={'center'} height={'50%'}></Logo>
+                <Logo 
+                    flex={1} 
+                    flexDirection={'column'} 
+                    justifyContent={'center'} 
+                    alignSelf={'center'} 
+                    height={'76px'}
+                    width={'263px'}
+                />
             </Pressable>
         </ImageBackground> 
       </View>
